5e520550ab4de6a41e5baaa7d2854559935fb0d4
Brandon <brandonluong2494@gmail.com>
2022-09-04 17:10:04 -0700

    first commit

---
+++ Scanner/scanner.h
@@ -0,0 +1,115 @@
+#ifndef SCANNER_H
+#define SCANNER_H
+#include <iostream>
+using namespace std;
+
+
+enum class Token
+{
+    INVALID,
+    EOF,
+
+    AND,
+    ARRAY,
+    ASM,
+    BEGIN,
+    BREAK,
+    CASE,
+    CONST,
+    //CONSTRUCTOR
+    //CONTINUE
+    //DESTRUCTOR
+    //DIV
+    DO,
+    //DOWNTO
+    ELSE,
+    END,
+    FALSE,
+    //FILE
+    FOR,
+    FUNCTION,
+    //GOTO
+    IF,
+    //IMPLEMENTATION
+    //IN
+    //INLINE
+    //INTERFACE
+    //LABEL
+    //MOD
+    //NIL
+    NOT,
+    //OBJECT
+    OF,
+    ON,
+    OPERATOR,
+    OR,
+    //PACKED,
+    //PROCEDURE,
+    //PROGRAM,
+    //RECORD,
+    //REPEAT,
+    //SET,
+    //SHL,
+    //SHR,
+    STRING,
+    THEN,
+    TO,
+    TRUE,
+    //TYPE,
+    UNIT,
+    UNTIL,
+    USES,
+    VAR,
+    WHILE,
+    WITH,
+    XOR,
+    INTEGER,
+    REAL,
+    IDENTIFIER,
+    PLUSOP,
+    MINUSOP,
+    MULTOP,
+    DIVOP,
+    ASSIGN,
+    EQUAL,
+    NE,
+    LTEQ,
+    GTEQ,
+    LT,
+    GT,
+    PLUSEQUAL,
+    MINUSEQUAL,
+    MULTEQUAL,
+    DIVEQUAL,
+    CARAT,
+    SEMICOLOR,
+    COMMA,
+    LPAREN,
+    RPAREN,
+    LBRACKET,
+    RBRACKET,
+    LBRACE,
+    RBRACE,
+    LCOMMENT,
+    RCOMMENT,
+};
+
+
+
+
+
+
+class Scanner
+{
+    public:
+    Token nextToken();
+    private:
+    void skipWhiteSpace();
+};
+
+
+
+
+
+
+#endif
---
+++ Source.h
@@ -0,0 +1,19 @@
+#ifndef SOURCE_H_
+#define SOURCE_H_
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+
+
+
+//Reads the source file
+
+
+
+
+#endif
