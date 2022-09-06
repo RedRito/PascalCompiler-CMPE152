e89221fb46ce1b7441aee1503b3e6a390fdc5494
Brandon <brandonluong2494@gmail.com>
2022-09-05 19:39:47 -0700

    Debug wrong terminal input

--- TestProgram.cpp
+++ TestProgram.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    if(argc != 2)
+    {
+        cout << "RUN BY ./main \"FileName\"" << endl;
+        exit(1);
+    }
     string fileName = argv[1];
     FileReader *file = new FileReader(fileName);
     char currentCh = file -> getCurrentChar();
