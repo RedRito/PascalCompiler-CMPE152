0f64aa1b93057d3aeb365e2bbe340d9fc4e8ae6c
Brandon <brandonluong2494@gmail.com>
2022-09-06 23:08:57 -0700

    Scanner.h Update //Probably final one

--- Scanner/scanner.h
+++ Scanner/scanner.h
@@ -1,102 +1,10 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 #include <iostream>
-#include "..FileReader.h"
+#include <stack>
+#include "FileReader.h"
+#include "PToken.h"
 using namespace std;
-
-
-enum class Token
-{
-    INVALID,
-    EOF,
-
-    AND,
-    ARRAY,
-    ASM,
-    BEGIN,
-    BREAK,
-    CASE,
-    CONST,
-    //CONSTRUCTOR
-    //CONTINUE
-    //DESTRUCTOR
-    //DIV
-    DO,
-    //DOWNTO
-    ELSE,
-    END,
-    FALSE,
-    //FILE
-    FOR,
-    FUNCTION,
-    //GOTO
-    IF,
-    //IMPLEMENTATION
-    //IN
-    //INLINE
-    //INTERFACE
-    //LABEL
-    //MOD
-    //NIL
-    NOT,
-    //OBJECT
-    OF,
-    ON,
-    OPERATOR,
-    OR,
-    //PACKED,
-    //PROCEDURE,
-    //PROGRAM,
-    //RECORD,
-    //REPEAT,
-    //SET,
-    //SHL,
-    //SHR,
-    STRING,
-    THEN,
-    TO,
-    TRUE,
-    //TYPE,
-    UNIT,
-    UNTIL,
-    USES,
-    VAR,
-    WHILE,
-    WITH,
-    XOR,
-    INTEGER,
-    REAL,
-    IDENTIFIER,
-    PLUSOP,
-    MINUSOP,
-    MULTOP,
-    DIVOP,
-    ASSIGN,
-    EQUAL,
-    NE,
-    LTEQ,
-    GTEQ,
-    LT,
-    GT,
-    PLUSEQUAL,
-    MINUSEQUAL,
-    MULTEQUAL,
-    DIVEQUAL,
-    CARAT,
-    SEMICOLOR,
-    COMMA,
-    LPAREN,
-    RPAREN,
-    LBRACKET,
-    RBRACKET,
-    LBRACE,
-    RBRACE,
-    LCOMMENT,
-    RCOMMENT,
-};
-
-
-
 
 
 
@@ -105,9 +13,11 @@ class Scanner
     public:
     //Scans for the next token
     Token *nextToken();
+    void printError(Token *token, string msg);
+    void printTokens();
+    void printToFile(string fileName);
     private:
     FileReader *file;
-    char currentCh;
     //construtor
     //Constructs the scanner from a given file
     Scanner(FileReader *file);
