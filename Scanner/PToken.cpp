3396b4239022b3f02280bc55c63308bf028fffba
TszKoYu <102338944+MadderHatterTea@users.noreply.github.com>
2022-09-06 21:30:14 -0700

    reserved words and symbols map

--- Scanner/PToken.cpp
+++ Scanner/PToken.cpp
@@ -1,3 +1,93 @@
+//Initialize reserved words map:
+map<string, Token> Token::ReservedWord;
+void Token::initMaps()
+{
+    ReservedWord["AND"] = AND;
+    ReservedWord["ARRAY"] = ARRAY;
+    ReservedWord["ASM"] = ASM;
+    ReservedWord["BEGIN"] = BEGIN;
+    ReservedWord["BREAK"] = BREAK;
+    ReservedWord["CASE"] = CASE;
+    ReservedWord["CONST"] = CONST;
+    ReservedWord["CONSTRUCTOR"] = CONSTRUCTOR;
+    ReservedWord["CONTINUE"] = CONTINUE;
+    ReservedWord["DESTRUCTOR"] = DESTRUCTOR;
+    ReservedWord["DIV"] = DIV;
+    ReservedWord["DO"] = DO;
+    ReservedWord["DOWNTO"] = DOWNTO;
+    ReservedWord["ELSE"] = ELSE;
+    ReservedWord["END"] = END;
+    ReservedWord["FALSE"] = FALSE;
+    ReservedWord["FILE"] = FILE;
+    ReservedWord["FOR"] = FOR;
+    ReservedWord["FUNCTION"] = FUNCTION;
+    ReservedWord["GOTO"] = GOTO;
+    ReservedWord["IF"] = IF;
+    ReservedWord["IMPLEMENTATION"] = IMPLEMENTATION;
+    ReservedWord["IN"] = IN;
+    ReservedWord["INLINE"] = INLINE;
+    ReservedWord["INTERFACE"] = INTERFACE;
+    ReservedWord["LABEL"] = LABEL;
+    ReservedWord["MOD"] = MOD;
+    ReservedWord["NIL"] = NIL;
+    ReservedWord["NOT"] = NOT;
+    ReservedWord["OBJECT"] = OBJECT;
+    ReservedWord["OF"] = OF;
+    ReservedWord["ON"] = ON;
+    ReservedWord["OPERATOR"] = OPERATOR;
+    ReservedWord["OR"] = OR;
+    ReservedWord["PACKED"] = PACKED;
+    ReservedWord["PROCEDURE"] = PROCEDURE;
+    ReservedWord["PROGRAM"] = PROGRAM;
+    ReservedWord["RECORD"] = RECORD;
+    ReservedWord["REPEAT"] = REPEAT;
+    ReservedWord["SET"] = SET;
+    ReservedWord["SHL"] = SHL;
+    ReservedWord["SHR"] = SHR;
+    ReservedWord["STRING"] = STRING;
+    ReservedWord["THEN"] = THEN;
+    ReservedWord["TO"] = TO;
+    ReservedWord["TRUE"] = TRUE;
+    ReservedWord["TYPE"] = TYPE;
+    ReservedWord["UNIT"] = UNIT;
+    ReservedWord["UNTIL"] = UNTIL;
+    ReservedWord["USES"] = USES;
+    ReservedWord["VAR"] = VAR;
+    ReservedWord["WHILE"] = WHILE;
+    ReservedWord["WITH"] = WITH;
+    ReservedWord["XOR"] = XOR;
+}
+//Initialize symbols map:
+map<string, Token> Token::Symbols;
+void Token::initMaps()
+{
+    Symbols["PLUSOP"] = PLUSOP;
+    Symbols["MINUSOP"] = MINUSOP;
+    Symbols["MULTOP"] = MULTOP;
+    Symbols["DIVOP"] = DIVOP;
+    Symbols["ASSIGN"] = ASSIGN;
+    Symbols["EQUAL"] = EQUAL;
+    Symbols["NE"] = NE;
+    Symbols["LTEQ"] = LTEQ;
+    Symbols["GTEQ"] = GTEQ;
+    Symbols["LT"] = LT;
+    Symbols["GT"] = GT;
+    Symbols["PLUSEQUAL"] = PLUSEQUAL;
+    Symbols["MINUSEQUAL"] = MINUSEQUAL;
+    Symbols["MULTEQUAL"] = MULTEQUAL;
+    Symbols["DIVEQUAL"] = DIVEQUAL;
+    Symbols["CARAT"] = CARAT;
+    Symbols["SEMICOLOR"] = SEMICOLOR;
+    Symbols["COMMA"] = COMMA;
+    Symbols["LPAREN"] = LPAREN;
+    Symbols["RPAREN"] = RPAREN;
+    Symbols["LBRACKET"] = LBRACKET;
+    Symbols["RBRACKET"] = RBRACKET;
+    Symbols["LBRACE"] = LBRACE;
+    Symbols["RBRACE"] = RBRACE;
+    Symbols["LCOMMENT"] = LCOMMENT;
+    Symbols["RCOMMENT"] = RCOMMENT;
+}
 Token *Token::Word(char firstChar, Source *source)
 {
     Token *token = new Token(firstChar);
