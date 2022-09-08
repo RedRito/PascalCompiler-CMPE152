3bdfefb8879ba7012c3e8fc7de22728d7d4a6eb1
Brandon <brandonluong2494@gmail.com>
2022-09-07 21:35:12 -0700

    Updated Makefile

--- TestProgram.cpp
+++ TestProgram.cpp
@@ -1,19 +1,17 @@
 #include <string>
-#include "Scanner/scanner.cpp"
-#include "Scanner/FileReader.cpp"
-#include "Scanner/PToken.cpp"
+#include "Scanner/scanner.h"
+#include "Scanner/FileReader.h"
+#include "Scanner/PToken.h"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    
     if(argc != 2)
     {
         cout << "RUN BY ./main \"FileName\"" << endl;
         exit(1);
     }
     string fileName = argv[1];
-    
     FileReader *file = new FileReader(fileName);
     /*
     char currentCh = file -> getCurrentChar();
