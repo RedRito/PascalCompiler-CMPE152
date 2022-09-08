1d05ba06cf6a5e0390e33f946ce91b547510d239
hemanthj21 <99715332+hemanthj21@users.noreply.github.com>
2022-09-08 13:33:31 -0700

    Update scanner.h

--- Scanner/scanner.h
+++ Scanner/scanner.h
@@ -13,8 +13,11 @@ class Scanner
     public:
     //Scans for the next token
     Token *nextToken();
+    //Prints error message
     void printError(Token *token, string msg);
+    //Prints Scanned tokens
     void printTokens();
+    //Prints output to file
     void printToFile(string fileName);
     //construtor
     //Constructs the scanner from a given file
