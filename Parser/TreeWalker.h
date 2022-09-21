3e49a8a0e2b57636e5879bf92ce2f4737a6c1a30
hemanthj21 <99715332+hemanthj21@users.noreply.github.com>
2022-09-21 11:53:29 -0700

    Create TreeWalker.h

---
+++ Parser/TreeWalker.h
@@ -0,0 +1,32 @@
+
+#ifndef TREEWALKER_H_
+#define TREEPWALKER_H_
+
+#include <string>
+#include <vector>
+
+#include "Node.h"
+
+namespace intermediate {
+
+using namespace std;
+
+class ParseTreePrinter
+{
+private:
+    static const string INDENT_SIZE;
+
+    string indentation;  
+    string line;         
+
+public:
+    ParseTreePrinter() : indentation(""), line("") {}
+    void print(Node *node);
+
+private:
+    void printChildren(vector<Node *> children);
+    void printLine();
+};
+
+}
+#endif /* TREEWALKER_H_ */
