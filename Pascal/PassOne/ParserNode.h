d8a6795bda946776977a16b17e27b345ad285b84
TszKoYu <102338944+MadderHatterTea@users.noreply.github.com>
2022-11-04 21:19:58 -0700

    NT_Names map added

--- Pascal/PassOne/ParserNode.h
+++ Pascal/PassOne/ParserNode.h
@@ -90,7 +90,123 @@ static const string NODE_TYPE_STRINGS[] =
 };
 
 /*Map for Node type names*/
-static map<NodeType, string> NT_Names;
+vector<NodeType> Ntypes =
+{
+    NodeType::PROGRAM,
+    NodeType::COMPOUND,
+    NodeType::ASSIGN,
+    NodeType::WHILE,
+    NodeType::LOOP,
+    NodeType::TEST,
+    NodeType::FOR,
+    NodeType::IF,
+    NodeType::SWITCH,
+    NodeType::VAR,
+    NodeType::VAR_DECLARATION,
+    NodeType::INTEGER,
+    NodeType::WRITE,
+    NodeType::WRITELN,
+    NodeType::ADD,
+    NodeType::SUBTRACT,
+    NodeType::OR,
+    NodeType::MULTIPLY,
+    NodeType::DIVIDE,
+    NodeType::NEGATE,
+    NodeType::FLOAT_DIVIDE,
+    NodeType::AND,
+    NodeType::NOT,
+    NodeType::SELECT_BRANCH,
+    NodeType::SELECT_CONSTANTS,
+    NodeType::EQ,
+    NodeType::LT,
+    NodeType::GT,
+    NodeType::NE,
+    NodeType::LTEQ,
+    NodeType::GTEQ,
+    NodeType::VARIABLE,
+    NodeType::INTEGER_CONSTANT,
+    NodeType::REAL_CONSTANT,
+    NodeType::STRING_CONSTANT,
+    NodeType::BOOLEAN_CONSTANT,
+};
+vector<string> NtypesName =
+{
+    "PROGRAM",
+    "COMPOUND",
+    "ASSIGN",
+    "WHILE",
+    "LOOP",
+    "TEST",
+    "FOR",
+    "IF",
+    "SWITCH",
+    "VAR",
+    "VAR_DECLARATION",
+    "INTEGER",
+    "WRITE",
+    "WRITELN",
+    "ADD",
+    "SUBTRACT",
+    "OR",
+    "MULTIPLY",
+    "DIVIDE",
+    "NEGATE",
+    "FLOAT_DIVIDE",
+    "AND",
+    "NOT",
+    "SELECT_BRANCH",
+    "SELECT_CONSTANTS",
+    "EQ",
+    "LT",
+    "GT",
+    "NE",
+    "LTEQ",
+    "GTEQ",
+    "VARIABLE",
+    "INTEGER_CONSTANT",
+    "REAL_CONSTANT",
+    "STRING_CONSTANT",
+    "BOOLEAN_CONSTANT"
+};
+static map<NodeType, string> NT_Names =
+{
+    {NodeType::PROGRAM, "PROGRAM"},
+    {NodeType::COMPOUND, "COMPOUND"},
+    {NodeType::ASSIGN, "ASSIGN"},
+    {NodeType::WHILE, "WHILE"},
+    {NodeType::LOOP, "LOOP"},
+    {NodeType::TEST, "TEST"},
+    {NodeType::FOR, "FOR"},
+    {NodeType::IF, "IF"},
+    {NodeType::SWITCH, "SWITCH"},
+    {NodeType::VAR, "VAR"},
+    {NodeType::VAR_DECLARATION, "VAR_DECLARATION"},
+    {NodeType::INTEGER, "INTEGER"},
+    {NodeType::WRITE, "WRITE"},
+    {NodeType::WRITELN, "WRITELN"},
+    {NodeType::ADD, "ADD"},
+    {NodeType::SUBTRACT, "SUBTRACT"},
+    {NodeType::OR, "OR"},
+    {NodeType::MULTIPLY, "MULTIPLY"},
+    {NodeType::DIVIDE, "DIVIDE"},
+    {NodeType::NEGATE, "NEGATE"},
+    {NodeType::FLOAT_DIVIDE, "FLOAT_DIVIDE"},
+    {NodeType::AND, "AND"},
+    {NodeType::NOT, "NOT"},
+    {NodeType::SELECT_BRANCH, "SELECT_BRANCH"},
+    {NodeType::SELECT_CONSTANTS, "SELECT_CONSTANTS"},
+    {NodeType::EQ, "EQ"},
+    {NodeType::LT, "LT"},
+    {NodeType::GT, "GT"},
+    {NodeType::NE, "NE"},   //Not Equal
+    {NodeType::LTEQ, "LTEQ"},
+    {NodeType::GTEQ, "GTEQ"},
+    {NodeType::VARIABLE, "VARIABLE"},
+    {NodeType::INTEGER_CONSTANT, "INTEGER_CONSTANT"},
+    {NodeType::REAL_CONSTANT, "rEAL_CONSTANT"},
+    {NodeType::STRING_CONSTANT, "STRING_CONSTANT"},
+    {NodeType::BOOLEAN_CONSTANT, "BOOLEAN_CONSTANT"},
+};
 
 class ParserNode
 {
@@ -106,10 +222,7 @@ public:
 
 
     //values of the node
-    double NodeValueReal;
-    int NodeValueInt;
-    string NodeValueString;
-    bool NodeValueBoolean;
+    object NodeObj;
     
     //list of node children
     vector<ParserNode*> childrenList;
